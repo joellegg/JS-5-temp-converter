35b8dfa47e9b7173ce0eab9dcf9542e2b756e8a8
joellegg <joellegg@bellsouth.net>
2016-11-30 13:59:03 -0600

    added js to determine which conversion to use

--- converter.js
+++ converter.js
@@ -1,9 +1,13 @@
 function toCelsius () {
-
+	// math to convert from fahrenheit to celcius
+	var temp_c = (temp_input - 32) * (5/9)
+	return temp_c;
 }
 
 function toFahrenheit () {
-
+	// math to convert celcius to fahrenheit
+	var temp_f = (temp_input * (9/5)) + 32;
+	return temp_f; 
 }
 
 // Get a reference to the button element in the DOM
@@ -12,7 +16,14 @@ var button = document.getElementById("converter");
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
+	var temp = document.getElementById('temp_input').value;
+	if (document.getElementById('fahrenheit').checked) {
+		toFahrenheit(temp);
+	} else if (document.getElementById('celcius').checked) {
+		toCelsius(temp);
+	} else {
+		alert("Select a unit to convert to.")
+	}
 }
 
 // Assign a function to be executed when the button is clicked
